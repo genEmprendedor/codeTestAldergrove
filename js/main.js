1b1adb883e3e926b28056f6fc4c45f54120d7a14
genEmprendedor <46355001+genEmprendedor@users.noreply.github.com>
2019-11-18 09:04:48 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(function($) {
+
+  "use strict";
+
+  /*=========================== preloader ===========================*/
+  // Wait for window load
+  $(window).on('load', function() {
+     $(".se-pre-con").fadeOut("slow");;
+  });
+
+/*=========================== preloader ===========================*/
+
+
+ /*=========================== click to section ===========================*/
+ //smooth scrool
+   // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $(".navbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+
+   //smooth scrool
+   // Add scrollspy to <body>
+  $('body').scrollspy({target: ".footer_menus", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $(".footer_menus a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+  /*=========================== click to section ===========================*/
+
+
+});
+
